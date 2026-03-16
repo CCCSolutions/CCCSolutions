598900e79e4732190684f14260b591909553c74a
William Yang <willi64645@gmail.com>
2026-03-16 17:21:54 -0400

    refactor: navbar and other hero page changes

--- website/components/Navbar.tsx
+++ website/components/Navbar.tsx
@@ -36,10 +36,9 @@ const Navbar = () => {
     <nav className="bg-white shadow-lg">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          <Link href="https://mmhs.ca" className="flex items-center">
+          <div className="flex items-center">
             <Image src="/images/mmhs_logo.png" alt="MMHS Logo" width={40} height={40} className="h-10 w-auto mr-3" />
-            <span className="text-xl font-medium text-gray-900">Milliken Mills High School</span>
-          </Link>
+          </div>
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
               {navItems.map((item) => (
