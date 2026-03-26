21454bd484304418366ed0c7793ce23938b51e84
William Yang <willi64645@gmail.com>
2026-03-26 01:52:39 -0400

    fix: remove navbar shadow for cleaner look

--- website/components/Navbar.tsx
+++ website/components/Navbar.tsx
@@ -33,7 +33,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="bg-white shadow-lg">
+    <nav className="bg-white">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <Link href="/" className="flex items-center">
